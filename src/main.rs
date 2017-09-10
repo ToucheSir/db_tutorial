4e848c1629f4afd09f2b2d09a53de65fcff6f658
Brian Chen <brianchen.czq@gmail.com>
2017-09-10 11:32:42 -0700

    Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
+use std::io::prelude::*;
+
+fn print_prompt() {
+    print!("db > ");
+    io::stdout().flush().unwrap();
+}
+
 fn main() {
     println!("Hello, world!");
+    let stdin = io::stdin();
+    let lines = stdin.lock().lines();
+
+    print_prompt();
+    for line in lines {
+        if let Ok(input) = line {
+            if input.starts_with(".exit") {
+                break
+            } else {
+                println!("Unrecognized command '{}'", input);
+            }
+        }
+        print_prompt();
+    }
 }
