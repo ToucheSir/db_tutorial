c84ee47a708c8e863d44412ab675a4d8b3d128cc
Brian Chen <brianchen.czq@gmail.com>
2017-09-10 12:18:08 -0700

    Part 2

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,43 @@ fn print_prompt() {
     io::stdout().flush().unwrap();
 }
 
+enum MetaCommand {
+    Exit
+}
+
+fn do_meta_command(command: &str) -> Result<MetaCommand, ParseError> {
+    if command.starts_with(".exit") {
+        Ok(MetaCommand::Exit)
+    } else {
+        Err(ParseError::Unrecognized)
+    }
+}
+
+enum ParseError {
+    Unrecognized
+}
+
+enum Statement {
+    Insert, Select
+}
+
+fn prepare_statement(input: &str) -> Result<Statement, ParseError> {
+    if input.starts_with("insert") {
+        Ok(Statement::Insert)
+    }  else if input.starts_with("select") {
+        Ok(Statement::Select)
+    } else {
+        Err(ParseError::Unrecognized)
+    }
+}
+
+fn execute_statement(statement: Statement) {
+  match statement {
+    Statement::Insert => println!("This is where we would do an insert."),
+    Statement::Select => println!("This is where we would do a select.")
+  }
+}
+
 fn main() {
     println!("Hello, world!");
     let stdin = io::stdin();
@@ -14,10 +51,19 @@ fn main() {
     print_prompt();
     for line in lines {
         if let Ok(input) = line {
-            if input.starts_with(".exit") {
-                break
-            } else {
-                println!("Unrecognized command '{}'", input);
+            if let Some('.') = input.chars().next() {
+                match do_meta_command(&input) {
+                    Ok(MetaCommand::Exit) => break,
+                    Err(ParseError::Unrecognized) => println!("Unrecognized command '{}'", input)
+                }
+            }
+            
+            match prepare_statement(&input) {
+                Ok(statement) => {
+                    execute_statement(statement);
+                    println!("Executed.")
+                }
+                Err(ParseError::Unrecognized) => println!("Unrecognized keyword at start of {}", input),
             }
         }
         print_prompt();
