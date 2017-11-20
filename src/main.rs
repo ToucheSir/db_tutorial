f91013590de904eb792f3e3252a77f058ff3c6c9
Brian Chen <brianchen.czq@gmail.com>
2017-11-19 17:39:28 -0800

    Part 6 (+ bugfixes)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 #![feature(const_size_of)]
+extern crate byteorder;
+
 use std::io;
 use std::io::prelude::*;
 use std::cmp;
 use std::fmt;
 use std::str;
+use std::env;
+use std::fs::{File, OpenOptions};
+use byteorder::{ReadBytesExt, WriteBytesExt, LE};
 
 const MAX_UNAME_LENGTH: usize = 32;
 const MAX_EMAIL_LENGTH: usize = 256;
@@ -16,6 +21,152 @@ struct Row {
     email_len: u32,
     email: [u8; MAX_EMAIL_LENGTH],
 }
+const ROW_SIZE: usize = std::mem::size_of::<Row>();
+
+#[derive(Debug)]
+enum PagerError {
+    OutOfBounds { page_num: usize, max_pages: usize },
+    CouldNotRead,
+}
+
+struct Pager {
+    // f: io::BufReader<File>,
+    fd: File,
+    file_size: u64,
+    pages: [Option<Box<Page>>; TABLE_MAX_PAGES],
+}
+
+impl Pager {
+    fn new(f: File, file_size: u64) -> Self {
+        Pager {
+            fd: f,
+            file_size,
+            pages: {
+                let mut array: [Option<Box<Page>>; 100] = unsafe { std::mem::uninitialized() };
+                for i in array.iter_mut() {
+                    unsafe {
+                        ::std::ptr::write(i, None);
+                    }
+                }
+                array
+            },
+        }
+    }
+
+    fn open(filename: &str) -> Result<Box<Self>, io::Error> {
+        let mut f = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(filename)?;
+        let file_size = f.seek(io::SeekFrom::End(0))
+            .expect("Could not read to end of file");
+        let pager = Box::new(Pager::new(f, file_size));
+        Ok(pager)
+    }
+
+    fn read_into_row<R: Read>(&self, reader: &mut R, row: &mut Row) -> io::Result<()> {
+        row.id = reader.read_u32::<LE>()?;
+        row.username_len = reader.read_u32::<LE>()?;
+        reader.read(&mut row.username)?;
+        row.email_len = reader.read_u32::<LE>()?;
+        reader.read(&mut row.email)?;
+        Ok(())
+    }
+
+    fn get_page<'a>(&'a mut self, page_num: usize) -> Result<&'a Page, PagerError> {
+        if page_num > TABLE_MAX_PAGES {
+            Err(PagerError::OutOfBounds {
+                page_num,
+                max_pages: TABLE_MAX_PAGES,
+            })
+        } else {
+            match self.pages[page_num] {
+                Some(ref page) => Ok(page),
+                None => {
+                    let mut new_page = self.allocate_page(page_num as u64).unwrap();
+                        // .map_err(|_| PagerError::CouldNotRead)?;
+                    self.pages[page_num] = Some(new_page);
+                    Ok(self.pages[page_num].as_ref().unwrap())
+                }
+            }
+        }
+    }
+
+    fn allocate_page(&mut self, page_num: u64) -> io::Result<Box<Page>> {
+        let mut new_page = Box::new(Page::new());
+        let num_pages = self.file_size / PAGE_SIZE + ((self.file_size % PAGE_SIZE != 0) as u64);
+
+        if (page_num as u64) < num_pages {
+            self.fd.seek(io::SeekFrom::Start(page_num as u64 * PAGE_SIZE))?;
+            let mut row_bytes = [0u8; ROW_SIZE];
+            for row in new_page.rows.iter_mut() {
+                match self.fd.read(&mut row_bytes) {
+                    Ok(ROW_SIZE) => self.read_into_row(&mut row_bytes.as_ref(), row)?,
+                    Ok(0) => break,
+                    Ok(_) => return Err(io::Error::from(io::ErrorKind::UnexpectedEof)),
+                    Err(e) => return Err(e)
+                }
+            }
+        }
+        Ok(new_page)
+    }
+
+    fn get_page_mut<'a>(&'a mut self, page_num: usize) -> Result<&'a mut Page, PagerError> {
+        match self.pages[page_num] {
+            Some(ref mut page) => Ok(page),
+            None => {
+                let mut new_page = self.allocate_page(page_num as u64)
+                    .map_err(|_| PagerError::CouldNotRead)?;
+                self.pages[page_num] = Some(new_page);
+                Ok(self.pages[page_num].as_mut().unwrap())
+                // match self.pages[page_num as usize] {
+                //     Some(ref mut page) => page.borrow_mut(),
+                //     _ => unreachable!(),
+                // }
+            }
+        }
+    }
+
+    // FIXME get rid of this
+    fn flush_partial(&mut self, page_num: usize, rows: usize) -> Result<(), io::Error> {
+        if let Some(ref page) = self.pages[page_num] {
+            let mut out = io::BufWriter::with_capacity(ROW_SIZE * rows, &mut self.fd);
+            out.seek(io::SeekFrom::Start(page_num as u64 * PAGE_SIZE))
+                .unwrap();
+            for r in page.rows[..rows].iter() {
+                out.write_u32::<LE>(r.id)?;
+                out.write_u32::<LE>(r.username_len)?;
+                out.write(&r.username)?;
+                out.write_u32::<LE>(r.email_len)?;
+                out.write(&r.email)?;
+            }
+        }
+        Ok(())
+    }
+
+    fn flush_page(&mut self, page_num: usize) -> Result<(), io::Error> {
+        let mut out = io::BufWriter::with_capacity(PAGE_SIZE as usize, &mut self.fd);
+        if let Some(ref page) = self.pages[page_num] {
+            out.seek(io::SeekFrom::Start(page_num as u64 * PAGE_SIZE))
+                .unwrap();
+            for r in page.rows.iter() {
+                out.write_u32::<LE>(r.id)?;
+                out.write_u32::<LE>(r.username_len)?;
+                out.write(&r.username)?;
+                out.write_u32::<LE>(r.email_len)?;
+                out.write(&r.email)?;
+            }
+        }
+        Ok(())
+    }
+}
+
+// impl Drop for Pager {
+//     fn drop(&mut self) {
+//         for i in 0..self.pages.len() {}
+//     }
+// }
 
 impl Row {
     fn new(id: u32, username: &[u8], email: &[u8]) -> Self {
@@ -28,8 +179,8 @@ impl Row {
             email_len: email_len as u32,
             email: [0; MAX_EMAIL_LENGTH],
         };
-        row.username[..username_len].clone_from_slice(&username[..username_len]);
-        row.email[..email_len].clone_from_slice(&email[..email_len]);
+        row.username[..username_len].copy_from_slice(&username[..username_len]);
+        row.email[..email_len].copy_from_slice(&email[..email_len]);
         row
     }
 }
@@ -68,6 +219,7 @@ impl fmt::Display for Row {
 const ROWS_PER_PAGE: usize = 4096 / std::mem::size_of::<Row>();
 const TABLE_MAX_PAGES: usize = 100;
 const TABLE_MAX_ROWS: u32 = (ROWS_PER_PAGE * TABLE_MAX_PAGES) as u32;
+const PAGE_SIZE: u64 = std::mem::size_of::<Page>() as u64;
 
 #[derive(Copy, Clone)]
 struct Page {
@@ -91,55 +243,21 @@ impl Page {
 }
 
 struct Table {
-    pages: [Option<Box<Page>>; 100],
+    pager: Box<Pager>,
     num_rows: u32,
 }
 
 impl Table {
-    fn new() -> Self {
+    fn new(pager: Box<Pager>) -> Self {
         Table {
-            pages: {
-                let mut array: [Option<Box<Page>>; 100] = unsafe { std::mem::uninitialized() };
-                for i in array.iter_mut() {
-                    unsafe {
-                        ::std::ptr::write(i, None);
-                    }
-                }
-                array
-            },
-            num_rows: 0,
-        }
-    }
-
-    fn get_page<'a>(&'a mut self, page_num: usize) -> &'a Page {
-        match self.pages[page_num] {
-            Some(ref page) => page,
-            None => {
-                let new_page = Box::new(Page::new());
-                self.pages[page_num] = Some(new_page);
-                self.pages[page_num].as_ref().unwrap()
-            }
-        }
-    }
-
-    fn get_page_mut<'a>(&'a mut self, page_num: usize) -> &'a mut Page {
-        match self.pages[page_num] {
-            Some(ref mut page) => page,
-            None => {
-                let new_page = Box::new(Page::new());
-                self.pages[page_num] = Some(new_page);
-                self.pages[page_num].as_mut().unwrap()
-                // match self.pages[page_num as usize] {
-                //     Some(ref mut page) => page.borrow_mut(),
-                //     _ => unreachable!(),
-                // }
-            }
+            num_rows: (pager.file_size / ROW_SIZE as u64) as u32,
+            pager,
         }
     }
 
     fn row_slot<'a>(&'a mut self, row_num: usize) -> &'a Row {
         let (page_num, row_offset) = (row_num / ROWS_PER_PAGE, row_num % ROWS_PER_PAGE);
-        let page = self.get_page(page_num);
+        let page = self.pager.get_page(page_num).unwrap();
         page.get_row(row_offset)
     }
 
@@ -148,7 +266,7 @@ impl Table {
             Err(ExecuteError::TableFull)
         } else {
             let (page_num, row_offset) = (row_num / ROWS_PER_PAGE, row_num % ROWS_PER_PAGE);
-            let page = self.get_page_mut(page_num);
+            let page = self.pager.get_page_mut(page_num).unwrap();
             page.set_row(row_offset, row);
             Ok(())
         }
@@ -185,13 +303,19 @@ fn prepare_statement(input: &str) -> Result<Statement, ParseError> {
         let _ = tokens.next(); // skip "insert"
         match (tokens.next(), tokens.next(), tokens.next()) {
             (Some(id_str), Some(username), Some(email)) => {
-                let id = id_str.parse::<i32>().map_err(|_| ParseError::InvalidSyntax)?;
-                if id < 0 { return Err(ParseError::NegativeID) }
+                let id = id_str
+                    .parse::<i32>()
+                    .map_err(|_| ParseError::InvalidSyntax)?;
+                if id < 0 {
+                    return Err(ParseError::NegativeID);
+                }
                 let (uname_bytes, email_bytes) = (username.as_bytes(), email.as_bytes());
                 if uname_bytes.len() > MAX_UNAME_LENGTH || email_bytes.len() > MAX_EMAIL_LENGTH {
-                    return Err(ParseError::StringTooLong)
-                } 
-                Ok(Statement::Insert(Row::new(id as u32, uname_bytes, email_bytes)))
+                    return Err(ParseError::StringTooLong);
+                }
+                Ok(Statement::Insert(
+                    Row::new(id as u32, uname_bytes, email_bytes),
+                ))
             }
             _ => Err(ParseError::InvalidSyntax),
         }
@@ -208,9 +332,9 @@ enum ExecuteError {
 
 fn execute_insert(row: &Row, table: &mut Table) -> Result<(), ExecuteError> {
     let num_rows = table.num_rows as usize;
-    let res = table.insert_row(row, num_rows);
+    table.insert_row(row, num_rows)?;
     table.num_rows += 1;
-    res
+    Ok(())
 }
 
 fn execute_select(table: &mut Table) -> Result<(), ExecuteError> {
@@ -232,8 +356,41 @@ fn print_prompt() {
     io::stdout().flush().unwrap();
 }
 
+fn db_open(filename: &str) -> Result<Table, io::Error> {
+    let pager = Pager::open(filename)?;
+    let table = Table::new(pager);
+    Ok(table)
+}
+
+fn db_close(table: &mut Table) -> Result<(), io::Error> {
+    let full_pages = table.num_rows as usize / ROWS_PER_PAGE;
+    for i in 0..full_pages {
+        table.pager.flush_page(i)?;
+    }
+    let remaining_rows = table.num_rows as usize % ROWS_PER_PAGE;
+    if remaining_rows > 0 {
+        table.pager.flush_partial(full_pages, remaining_rows)?;
+    }
+    Ok(())
+}
+
 fn main() {
-    let mut table = Table::new();
+    let mut args = env::args();
+    let filename = match (args.next(), args.next()) {
+        (_, None) => {
+            println!("Must supply a database filename.");
+            std::process::exit(1);
+        }
+        (_, Some(filename)) => filename,
+    };
+
+    let mut table = match db_open(&filename) {
+        Ok(t) => t,
+        Err(e) => {
+            println!("Could not open file {}: {}", filename, e);
+            std::process::exit(1);
+        }
+    };
 
     let stdin = io::stdin();
     let lines = stdin.lock().lines();
@@ -253,21 +410,20 @@ fn main() {
                 Ok(statement) => match execute_statement(statement, &mut table) {
                     Ok(()) => println!("Executed."),
                     Err(ExecuteError::TableFull) => println!("Error: Table full."),
-                }
+                },
                 Err(ParseError::Unrecognized) => {
                     println!("Unrecognized keyword at start of {}", input)
                 }
-                Err(ParseError::NegativeID) => {
-                    println!("ID must be positive.")
-                }
-                Err(ParseError::StringTooLong) => {
-                    println!("String is too long.")
-                }
+                Err(ParseError::NegativeID) => println!("ID must be positive."),
+                Err(ParseError::StringTooLong) => println!("String is too long."),
                 Err(ParseError::InvalidSyntax) => {
                     println!("Syntax error: could not parse statement.")
                 }
             }
         }
         print_prompt();
+    }
+    if let Err(e) = db_close(&mut table) {
+        println!("Could not close db {}: {}", filename, e);
     }
 }
--- test.js
+++ test.js
@@ -1,110 +1,97 @@
-const fs = require('fs');
-const {execFileSync} = require('child_process');
-const assert = require('assert');
+const fs = require("fs");
+const { execFileSync } = require("child_process");
+const assert = require("assert");
 
-describe('database', () => {
-  before(() => {
-      try {
-        fs.unlinkSync('test.db')
-      } catch (_) { }
-  })
+describe("database", () => {
+  beforeEach(() => {
+    try {
+      fs.unlinkSync("./test.db");
+    } catch (_) {}
+  });
 
   function run_script(commands) {
-    const output = execFileSync("./target/debug/db_tutorial", {
-        input: commands.join('\n'),
-        env: { RUST_BACKTRACE: 1 }
-    })
-    return output.toString().split("\n")
+    const output = execFileSync("./target/debug/db_tutorial", ["./test.db"], {
+      input: commands.join("\n"),
+      env: { RUST_BACKTRACE: 1 }
+    });
+    return output.toString().split("\n");
   }
 
-  it('inserts and retreives a row', () => {
+  it("inserts and retreives a row", () => {
     const result = run_script([
       "insert 1 user1 person1@example.com",
       "select",
-      ".exit",
-    ])
+      ".exit"
+    ]);
     assert.deepEqual(result, [
       "db > Executed.",
       "db > (1, user1, person1@example.com)",
       "Executed.",
-      "db > ",
-    ])
-  })
+      "db > "
+    ]);
+  });
 
-  // TODO: part 5
-  // it('keeps data after closing connection', () => {
-  //   const result1 = run_script([
-  //     "insert 1 user1 person1@example.com",
-  //     ".exit",
-  //   ])
-  //   assert.deepEqual(result1, [
-  //     "db > Executed.",
-  //     "db > ",
-  //   ])
+  it("keeps data after closing connection", () => {
+    const result1 = run_script(["insert 1 user1 person1@example.com", ".exit"]);
+    assert.deepEqual(result1, ["db > Executed.", "db > "]);
 
-  //   const result2 = run_script([
-  //     "select",
-  //     ".exit",
-  //   ])
-  //   assert.deepEqual(result2, [
-  //     "db > (1, user1, person1@example.com)",
-  //     "Executed.",
-  //     "db > ",
-  //   ])
-  // })
+    const result2 = run_script(["select", ".exit"]);
+    assert.deepEqual(result2, [
+      "db > (1, user1, person1@example.com)",
+      "Executed.",
+      "db > "
+    ]);
+  });
 
-  it('prints error message when table is full', () => {
-    const script = Array.from(Array(1400).keys())
-        .map(i => `insert ${i + 1} user${i + 1} person${i + 1}@example.com`)
-    script.push(".exit")
-    const result = run_script(script)
-    assert.deepEqual(result[result.length-2], 'db > Error: Table full.')
-  })
+  it("prints error message when table is full", () => {
+    const script = Array.from(Array(1400).keys()).map(
+      i => `insert ${i + 1} user${i + 1} person${i + 1}@example.com`
+    );
+    script.push(".exit");
+    const result = run_script(script);
+    assert.deepEqual(result[result.length - 2], "db > Error: Table full.");
+  });
 
-  it('allows inserting strings that are the maximum length', () => {
-    const long_username = 'a'.repeat(32)
-    const long_email = 'a'.repeat(256)
+  it("allows inserting strings that are the maximum length", () => {
+    const long_username = "a".repeat(32);
+    const long_email = "a".repeat(256);
     const script = [
       "insert 1 #{long_username} #{long_email}",
       "select",
-      ".exit",
-    ]
-    const result = run_script(script)
+      ".exit"
+    ];
+    const result = run_script(script);
     assert.deepEqual(result, [
       "db > Executed.",
       "db > (1, #{long_username}, #{long_email})",
       "Executed.",
-      "db > ",
-    ])
-  })
+      "db > "
+    ]);
+  });
 
-  it('prints error message if strings are too long', () => {
-    const long_username = 'a'.repeat(33)
-    const long_email = 'a'.repeat(256)
+  it("prints error message if strings are too long", () => {
+    const long_username = "a".repeat(33);
+    const long_email = "a".repeat(256);
     const script = [
       `insert 1 ${long_username} ${long_email}`,
       "select",
-      ".exit",
-    ]
-    const result = run_script(script)
+      ".exit"
+    ];
+    const result = run_script(script);
     assert.deepEqual(result, [
       "db > String is too long.",
       "db > Executed.",
-      "db > ",
-    ])
+      "db > "
+    ]);
   });
 
-  it('prints an error message if id is negative', () => {
-    const script = [
-      "insert -1 cstack foo@bar.com",
-      "select",
-      ".exit",
-    ]
-    const result = run_script(script)
+  it("prints an error message if id is negative", () => {
+    const script = ["insert -1 cstack foo@bar.com", "select", ".exit"];
+    const result = run_script(script);
     assert.deepEqual(result, [
       "db > ID must be positive.",
       "db > Executed.",
-      "db > ",
-    ])
-  })
-})
+      "db > "
+    ]);
+  });
+});
